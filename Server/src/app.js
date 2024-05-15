2e21331f32c69470abdb5fbd8b2b76a7045e0251
edison <evasquez16.ev@gmail.com>
2024-05-15 16:56:40 -0500

    Application stable version

--- Server/src/app.js
+++ Server/src/app.js
@@ -9,7 +9,13 @@ const cashRoutes = require('./routes/cashPayment')
 const ccRoutes = require('./routes/creaditCardPayment')
 const swaggerConfig = require('./config/swaggerConfig')
 const morgan = require('morgan')
-const config = require('./config/dbports')
+const config = require('./config/ports')
+const configdb = require('./config/dbports')
+const environment = process.env.environment || 'dev'; 
+const port = config[environment]
+const portdb = configdb[environment]
+const pathdb = process.env.MONGO_BASE + ":" + portdb + process.env.MONGO_NAME
+
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
@@ -24,18 +30,11 @@ app.use((req, res) => {
     res.status(404).render('404', {title: 'Page not found'})
 })
 
-const environment = process.env.environment || 'dev'; 
-const port = config[environment]
-
-
-const pathdb = process.env.MONGO_BASE + environment + ":" + port + process.env.MONGO_NAME
-
-console.log('Connect to MongoDB for path:', pathdb)
 mongoose
 .connect(pathdb, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB Successfull!'))
+    .then(() => console.log('Connected to MongoDB for path:', pathdb))
     .catch((error) => (console.error(error)))
 
-app.listen(port, () => console.log('Server listening environment and port: ', environment, port))
+app.listen(port, () => console.log('Server listening environment and port', environment, port))
 
 module.exports = router
